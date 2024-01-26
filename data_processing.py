e1632f1b6bdc1498d4ec9bb2745cce124b1d6a1f
Viktor Home PC <bolasovviktor@gmail.com>
2024-01-27 01:19:54 +0700

    feat: replace functions with classes

--- data_processing.py
+++ data_processing.py
@@ -4,95 +4,114 @@ import queue
 from time import sleep
 
 
-def genrator(data_queue):
-    for i in range(5):
-        sleep(1)
-        data_queue.put(i)
+class GeneratorData():
+    def __init__(self, count_generated_data, data_queue):
+        self.count_data = count_generated_data
+        self.queue = data_queue
 
-        print(f'genrator: genrate {i}')
-        
-    print(f'genrator: done')
+    def start_genertion(self, sleep_time=1):
+        for i in range(self.count_data):
+            sleep(sleep_time)
+            self.queue.put(i)
+            print(f'genrator: genrate {i}')
+        print(f'genrator: done')
+
+class ProcessorData():
+    def __init__(self, data_queue, storage_file):
+        self.queue = data_queue
+        self.storage = storage_file
+
+    def validate_data(self, data):
+        if type(data) is int:
+            return True
+        else:
+            return False
+
+    def run_processing(self, lock):
+        while True:
+            try:
+                data = self.queue.get(timeout=10.0)
+                print(f'processor: {data}')
+                is_valid_data = self.validate_data(data)
+
+                if is_valid_data:
+                    lock.acquire()
+                    with open(self.storage, "rb") as file:
+                        file_data = pickle.load(file)
+
+                    file_data.append(data)
+
+                    with open(self.storage, "wb") as file:
+                        pickle.dump(file_data, file)
+                    lock.release()
+
+                sleep(0.3)
+            except queue.Empty as e:
+                print(f'processor Done')
+                break
 
 
-def validate_data(data):
-    if type(data) is int:
-        return True
-    else:
-        return False 
+class SenderData():
+    def __init__(self, storage_file):
+        self.storage = storage_file
 
+    def send_to_imaginary_server(self, data):
+        pass
 
-def processor(lock, data_queue, file_to_save='./data.pickle'):
-    while True:
-        try:
-            data = data_queue.get(timeout=10.0)
-            print(f'processor: {data}')
-            is_valid_data = validate_data(data)
-
-            if is_valid_data:
-                lock.acquire()
-                with open("data.pickle", "rb") as file:
-                    file_data = pickle.load(file)
-
-                file_data.append(data)
-
-                with open("data.pickle", "wb") as file:
+    def run_sending(self, lock):
+        batch_num = 0
+        count_cycles_without_data = 0
+        while True:
+            sleep(3)
+            lock.acquire()
+            with open(self.storage, "rb") as file:
+                file_data = pickle.load(file)
+            if file_data:
+                data_to_send = file_data.copy()
+                file_data.clear()
+                with open(self.storage, "wb") as file:
                     pickle.dump(file_data, file)
                 lock.release()
 
-            sleep(0.3)
-        except queue.Empty as e:
-            print(f'processor Done')
-            break
+                for data in data_to_send:
+                    print(f"sending {data}...")
+                    self.send_to_imaginary_server(data)
+                    sleep(1)
+                    print(f"{data} sended!")
 
+                print(f'data batch num {batch_num} sended')
+                batch_num += 1
+                count_cycles_without_data = 0
+            else:
+                lock.release()
+                count_cycles_without_data += 1
 
-def send_to_imaginary_server(data):
-    pass
-
-
-def sender(lock, load_file='./data.pickle'):
-    batch_num = 0
-    count_cycles_without_data = 0
-    while True:
-        sleep(3)
-        lock.acquire()
-        with open("data.pickle", "rb") as file:
-            file_data = pickle.load(file)
-        if file_data:
-            data_to_send = file_data.copy()
-            file_data.clear()
-            with open("data.pickle", "wb") as file:
-                pickle.dump(file_data, file)
-            lock.release() 
-
-            for data in data_to_send:
-                print(f"sending {data}...")
-                send_to_imaginary_server(data)
-                sleep(1)
-                print(f"{data} sended!")
-
-            print(f'data batch num {batch_num} sended')
-            batch_num += 1
-            count_cycles_without_data = 0
-        else:
-            lock.release()
-            count_cycles_without_data += 1
-
-        if count_cycles_without_data == 5:
-            break
-    print("Sender done")
+            if count_cycles_without_data == 5:
+                break
+        print("Sender done")
 
 
 def main():
-    inital_lsit = []
-    with open("data.pickle", "wb") as file:
+    # отправить коммит с классами
+    # добавить аргументы и их валидацию
+    # добавить фэйкер
+    # добавить доку
+
+    storage_path = "./data.pickle"
+    inital_lsit=[]
+    with open(storage_path, "wb") as file:
         pickle.dump(inital_lsit, file)
 
     lock = multiprocessing.Lock()
     data_queue = multiprocessing.Queue()
 
-    p1 = multiprocessing.Process(target=genrator, args=(data_queue,))
-    p2 = multiprocessing.Process(target=processor, args=(lock, data_queue,))
-    p3 = multiprocessing.Process(target=sender, args=(lock,))
+    d_generator = GeneratorData(5, data_queue)
+    d_processor = ProcessorData(data_queue, storage_path)
+    d_sender = SenderData(storage_path)
+
+    p1 = multiprocessing.Process(target=d_generator.start_genertion, args=(1,))
+    p2 = multiprocessing.Process(target=d_processor.run_processing, args=(lock,))
+    p3 = multiprocessing.Process(target=d_sender.run_sending, args=(lock,))
 
     p1.start()
     p2.start()
@@ -110,5 +129,5 @@ def test_data():
 
 
 if __name__ == '__main__':
-    test_data()
-    # main()
+    # test_data()
+    main()
